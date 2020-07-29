ff35c1b594a079a203aeb9f1bcdae61dd2dcc7e1
Xuehua Chen <xuehua@gmail.com>
2020-07-28 22:08:09 -0700

    Added an author field.

--- django-pysolr/blog/models.py
+++ django-pysolr/blog/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
 class Blog(models.Model):
     title = models.CharField(max_length=100, blank=False, null=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     description = models.TextField(blank=False, null=False)
     summary = models.CharField(max_length=200, blank=False, null=False)
-
+    author = models.ForeignKey(get_user_model(), blank=True, null=True, related_name="blogs", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     class Meta:
         verbose_name = 'Blog'
         verbose_name_plural = 'Blogs'
