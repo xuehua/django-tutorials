0449b87f6081a5386d3670ae4ef52d38a47f2327
Xuehua Chen <xuehua@gmail.com>
2020-07-29 09:11:15 -0700

    Minor changes. Added blogger group in admin.

--- django-pysolr/users/admin.py
+++ django-pysolr/users/admin.py
@@ -3,13 +3,14 @@ from django.contrib.auth.admin import UserAdmin
 from users.models import CustomUser, Contact
 from users.forms import CustomUserCreationForm, CustomUserChangeForm
 
-
+#@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     pass
