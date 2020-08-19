97cda29e2e136b363ccb9a81e37757889416d785
xuehua <xuehua@gmail.com>
2020-08-19 11:51:24 -0700

    Added users_app.

---
+++ user_add/users_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class User(models.Model):
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.EmailField(max_length=255)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+
