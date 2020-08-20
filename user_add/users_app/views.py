8bdb5c5406326bfab2c2f528985858607caa86a6
xuehua <xuehua@gmail.com>
2020-08-19 17:08:29 -0700

    Added routing, forms and views.

--- user_add/users_app/views.py
+++ user_add/users_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404
+from .models import User
 
-# Create your views here.
+def user_list_view(request):
+    user_list = get_list_or_404(User)
+    return render(request, "user_list.html", {'user_list':user_list})
