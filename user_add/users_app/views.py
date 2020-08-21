33f20723bd94c51454dbb7f2d06a0829076407bc
xuehua <xuehua@gmail.com>
2020-08-20 20:49:19 -0700

    Added template, views and a model form.

--- user_add/users_app/views.py
+++ user_add/users_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_list_or_404
 from .models import User
-
+from .forms import UserForm
 def user_list_view(request):
     user_list = get_list_or_404(User)
-    return render(request, "user_list.html", {'user_list':user_list})
+    user_form = UserForm()
+    return render(request, "user_list.html", 
+                {'user_list':user_list, 
+                'user_form': user_form})
