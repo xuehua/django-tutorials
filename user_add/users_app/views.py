2887ac9a0541ed039090d1ae2a45d7f4c59124d5
xuehua <xuehua@gmail.com>
2020-08-20 21:14:04 -0700

    Added post method handling.

--- user_add/users_app/views.py
+++ user_add/users_app/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, redirect, get_list_or_404
 from .models import User
 from .forms import UserForm
+
 def user_list_view(request):
     user_list = get_list_or_404(User)
     user_form = UserForm()
+
+    if request.method == "POST":
+        user_form = UserForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect("user_list")
+
     return render(request, "user_list.html", 
                 {'user_list':user_list, 
                 'user_form': user_form})
