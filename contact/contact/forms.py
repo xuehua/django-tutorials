b13dba4d75bbbea6eff07bbd9f5d6422cfc3b522
Xuehua Chen <xuehua@gmail.com>
2020-11-30 20:43:02 -0800

    enhance layout.

--- contact/contact/forms.py
+++ contact/contact/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 
 class EmailPostForm(forms.Form):
-    first_name = forms.CharField(max_length=25,
-        widget=forms.TextInput(attrs={'placeholder': 'Enter First Name'}))
-    last_name = forms.CharField(max_length=25,
-        widget=forms.TextInput(attrs={'placeholder': 'Enter Last Name'}))
-    email = forms.EmailField(
-        widget=forms.EmailInput(attrs={'placeholder': 'Enter Email'}))
-    subject = forms.CharField(max_length=100, 
-        widget=forms.TextInput(attrs={'placeholder': 'Enter Subject'}))
-
-    comments = forms.CharField(widget=forms.Textarea)
+    first_name = forms.CharField(max_length=25, label="First Name:",
+        widget=forms.TextInput(attrs={'class':"form-control", 'placeholder': 'Enter First Name'}))
+    last_name = forms.CharField(max_length=25, label="Last Name:",
+        widget=forms.TextInput(attrs={'class':"form-control",'placeholder': 'Enter Last Name'}))
+    email = forms.EmailField(label="Email:",
+        widget=forms.EmailInput(attrs={'class':"form-control",'placeholder': 'Enter Email'}))
+    subject = forms.CharField(max_length=100, label="Subject:", 
+        widget=forms.TextInput(attrs={'class':"form-control",'placeholder': 'Enter Subject'}))
+    comments = forms.CharField(label="Comments:",
+        widget=forms.Textarea(attrs={'class':"form-control", "rows":5}))
     
