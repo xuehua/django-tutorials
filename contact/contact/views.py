a745fb7eb40f35ab56dcaac07ba05555c9950874
Xuehua Chen <xuehua@gmail.com>
2020-11-30 21:04:58 -0800

    Added logic to send email.

--- contact/contact/views.py
+++ contact/contact/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+
 from .forms import EmailPostForm
-# Create your views here.
+
+
 def post_comment(request):
+    sent = False
     if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            
+            subject = cd['subject']
+            message = cd['comments']
+            email = cd['email']
+            send_mail(subject, message, email, ['xuehua@gmail.com'])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, "contact.html", {'form': form})
+    return render(request, "contact.html", {'form': form, 'sent': sent})
