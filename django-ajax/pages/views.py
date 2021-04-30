29965bf96ea409043adfdf11d0e2ce241ab97079
Xuehua Chen <xuehua@gmail.com>
2021-04-30 11:06:36 -0700

    Added the ajax_required decorator.

--- django-ajax/pages/views.py
+++ django-ajax/pages/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from common.decorators import ajax_required
 # Create your views here.
 def homepage(request):
     return render(request, 'index.html')
 
+@ajax_required
 def ajax(request):
     info = {'top_track': 'Equilibrium', 'top_artist': 'Celine Dion'}
     return render(request, 'ajax.html', {'info':info})
