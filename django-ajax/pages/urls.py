e60dc0851b3a9eaa6a67914bd5b53fd9750f64a3
Xuehua Chen <xuehua@gmail.com>
2021-04-30 11:00:49 -0700

    Added simple ajax support.

--- django-ajax/pages/urls.py
+++ django-ajax/pages/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='home' ),
+    path('ajax/', views.ajax, name='ajax'),
 ]
--- django-ajax/pages/views.py
+++ django-ajax/pages/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, 'index.html')
+
+def ajax(request):
+    info = {'top_track': 'Equilibrium', 'top_artist': 'Celine Dion'}
+    return render(request, 'ajax.html', {'info':info})
